666721224bc2d88b24f426d92ad62ed4a94e7b23
Alvin Yanson <yanson.alvin.info@gmail.com>
2024-08-26 19:15:21 +0800

    feat: done in 05-Routing-Assignment

---
+++ 05-Routing-Assignment/Program.cs
@@ -0,0 +1,50 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+var countries = new Dictionary<int, string>
+{
+    { 1, "United States" },
+    { 2, "Canada" },
+    { 3, "United Kingdom" },
+    { 4, "India" },
+    { 5, "Japan" }
+};
+
+app.MapGet("/countries", async(HttpContext context) =>
+{
+    foreach(var country in countries)
+    {
+        await context.Response.WriteAsync($"{country.Key}, {country.Value}\n");
+    }
+});
+
+app.MapGet("/countries/{countryId:int:range(1, 100)}", async (HttpContext context) =>
+{
+    if(!context.Request.RouteValues.ContainsKey("countryId"))
+    {
+        context.Response.StatusCode = 400;
+        await context.Response.WriteAsync("No country id found");
+    }
+
+    int countryId = Convert.ToInt32(context.Request.RouteValues["countryId"]);
+
+    if(countries.ContainsKey(countryId))
+    {
+        string countryName = countries[countryId];
+
+        await context.Response.WriteAsync($"{countryName}");
+    }
+    else
+    {
+        context.Response.StatusCode = 404;
+        await context.Response.WriteAsync("No country match with this id.");
+    }
+});
+
+app.MapGet("/countries/{countryId:min(101)}", async (HttpContext context) =>
+{
+    context.Response.StatusCode = 400;
+    await context.Response.WriteAsync($"The CountryID should be between 1 and 100.");
+});
+
+app.Run();

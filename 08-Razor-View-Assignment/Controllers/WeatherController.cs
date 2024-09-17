235b87adb908fa06262c74e10596056d5e085e01
gitalvininfo <yanson.alvin.info@gmail.com>
2024-09-17 22:51:43 +0800

    feat: done in  08-Razor-View-Assignment

---
+++ 08-Razor-View-Assignment/Controllers/WeatherController.cs
@@ -0,0 +1,54 @@
+﻿using _08_Razor_View_Assignment.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _08_Razor_View_Assignment.Controllers
+{
+    public class WeatherController : Controller
+    {
+
+        List<WeatherReport> weatherReports = new List<WeatherReport>
+        {
+            new WeatherReport
+            {
+                CityUniqueCode = "LDN",
+                CityName = "London",
+                DateAndTime = DateTime.Parse("2030-01-01 8:00"),
+                TemperatureFahrenheit = 33
+            },
+            new WeatherReport
+            {
+                CityUniqueCode = "NYC",
+                CityName = "New York",
+                DateAndTime = DateTime.Parse("2030-01-01 3:00"),
+                TemperatureFahrenheit = 60
+            },
+            new WeatherReport
+            {
+                CityUniqueCode = "PAR",
+                CityName = "Paris",
+                DateAndTime = DateTime.Parse("2030-01-01 9:00"),
+                TemperatureFahrenheit = 82
+            }
+        };
+
+
+
+        public IActionResult Index()
+        {
+            return View(weatherReports);
+        }
+
+        [HttpGet("weather/{cityCode}")]
+        public IActionResult City(string cityCode)
+        {
+            var matchedCity = weatherReports.FirstOrDefault(weather => weather.CityUniqueCode == cityCode);
+
+            if (matchedCity != null)
+            {
+                return View(matchedCity);
+            }
+
+            return View();
+        }
+    }
+}
---
+++ 08-Razor-View-Assignment/Models/WeatherReport.cs
@@ -0,0 +1,10 @@
+﻿namespace _08_Razor_View_Assignment.Models
+{
+    public class WeatherReport
+    {
+        public string CityUniqueCode { get; set; }
+        public string CityName { get; set; }
+        public DateTime DateAndTime { get; set; }
+        public int TemperatureFahrenheit { get; set; }
+    }
+}
